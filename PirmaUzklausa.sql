427d625bfdf2a3e316fc4d4ecf27d3dc37ab148c
Vilenas11 <78947355+Vilenas11@users.noreply.github.com>
2022-03-24 13:02:00 +0200

    Update PirmaUzklausa.sql

--- PirmaUzklausa.sql
+++ PirmaUzklausa.sql
@@ -1,8 +1,6 @@
 /*
 Visų skaitytojų, kurių adrese yra konkreti frazė, pavardės, vardai ir adresai.
 */
-SELECT
-Ak, vardas, pavarde, gimimas
-from stud.skaitytojas
-where
-  DATE_PART('year', AGE('2021-10-23', gimimas)) > 18;
+SELECT vardas, pavarde, adresas
+FROM stud.skaitytojas
+WHERE Adresas Like '%Tiesioji%'; 
